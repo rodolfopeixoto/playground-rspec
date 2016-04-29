4d8abb892bb57b27536d268f36e170b77bbfacff
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-04-29 17:57:51 +0000

    Factored test with before do

--- spec/playground_spec.rb
+++ spec/playground_spec.rb
@@ -1,17 +1,20 @@
 require_relative '../lib/playground'
 
 describe Playground do
+    
+    before do
+      @playground = Playground.new(0)
+    end
+    
   context 'when there are no children' do
     it 'is quite boring place' do
     #expect(actual_value).to equal(expected_value)
-      playground = Playground.new(0)
-      mood = playground.mood
+      mood = @playground.mood
       expect(mood).to eq('boring')
    end
     
     it 'is empty' do
-        playground = Playground.new(0)
-      expect(playground).to be_empty
+      expect(@playground).to be_empty
     end
   end
 end
