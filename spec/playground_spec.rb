e25f82e5bc765d939c4c1b2147952aadbcc3cae5
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-04-29 17:59:37 +0000

    Removed variable instance and added let

--- spec/playground_spec.rb
+++ spec/playground_spec.rb
@@ -5,16 +5,17 @@ describe Playground do
     before do
       @playground = Playground.new(0)
     end
+    let(:playground) { Playground.new(0) }
     
   context 'when there are no children' do
     it 'is quite boring place' do
     #expect(actual_value).to equal(expected_value)
-      mood = @playground.mood
+      mood = playground.mood
       expect(mood).to eq('boring')
    end
     
     it 'is empty' do
-      expect(@playground).to be_empty
+      expect(playground).to be_empty
     end
   end
 end
