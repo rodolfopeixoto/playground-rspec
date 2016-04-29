89ee257e662addd6e77c239fd3ce0ef98f819870
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-04-29 17:50:45 +0000

    Created first test playground boring

--- spec/playground_spec.rb
+++ spec/playground_spec.rb
@@ -2,7 +2,13 @@ require_relative '../lib/playground'
 
 describe Playground do
   context 'when there are no children' do
-    it 'is quite boring place'
+    it 'is quite boring place' do
+    #expect(actual_value).to equal(expected_value)
+      playground = Playground.new(0)
+      mood = playground.mood
+      expect(mood).to eq('boring')
+   end
+    
     it 'is empty'
   end
 end
